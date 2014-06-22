87ad65ce95d4363444e420d79a8502dac15abad1
gottagitgit <gilboa.anat@gmail.com>
2014-06-21 18:00:14 -0700

    added some necessary changes.

--- static/index.js
+++ static/index.js
@@ -4,8 +4,10 @@ emoji.init_env();
 
 // Static Emoji rendering in page
 var footer = "Made with :green_heart: by <a target='_blank' href='https://twitter.com/jasdev'>@jasdev</a>";
-var instructions = "Enter an integer in the textarea below :point_down:";
+var second_footer = "Fixed with :two_hearts: by <a target='_blank' href='https://twitter.com/anat_gilboa'>@anat</a>";
+var instructions = "Enter an integer in the text area below :point_down:";
 document.getElementById('footer').innerHTML = emoji.replace_colons(footer);
+document.getElementById('second_footer').innerHTML = emoji.replace_colons(second_footer);
 document.getElementById('instructions').innerHTML = emoji.replace_colons(instructions);
 
 function parseEmoji() {
@@ -37,8 +39,18 @@ function stringToEmojiCode(input) {
     for(var i = 0; i < input.length; i++)
         if(input[i] === "-")
             out.push(':heavy_minus_sign:');
-        else
+        else if((input.length - i) >= 4 && input.substr(i,4) == "1234"){
+            out.push(':1234:');
+            i += 3;
+        } else if((input.length - i) >= 3 && input.substr(i,3) == "100"){
+            out.push(':100:');
+            i += 2;
+        } else if((input.length - i) >= 2 && input.substr(i,2) == "10"){
+            out.push(':keycap_ten:');
+            i += 1;
+        } else{
             out.push(digits[parseInt(input[i])]);
+        }
 
     return out.join("");
 }
